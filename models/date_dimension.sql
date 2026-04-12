05147e576f1d93283578718b8db5f38315c80605
Priya Y <priya.basel2021@gmail.com>
2026-04-12 19:08:50 +0000

    small error fixed

--- models/date_dimension.sql
+++ models/date_dimension.sql
@@ -1,10 +1,9 @@
 WITH CTE AS (
     SELECT
-        STARTED_AT,
-        TO_TIMESTAMP(STARTED_AT)
+        STARTED_AT
     FROM {{ source('demo', 'bike') }}
-    WHERE STARTED_AT != 'started_at'
 )
 
 SELECT *
-FROM CTE;
+FROM CTE
+
